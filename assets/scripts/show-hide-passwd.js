41219af495c29c83146f908bd5b602b61e4403d4
Matheus Pazinati <matheuspazinati@gmail.com>
2022-06-08 15:56:24 -0300

    Identa codigo

--- assets/scripts/show-hide-passwd.js
+++ assets/scripts/show-hide-passwd.js
@@ -1,17 +1,17 @@
 export function showHidePassword() {
-    const passwIcons = document.querySelectorAll('[data-icon]')
-    passwIcons.forEach((icon) => {
-        const input = icon.parentElement.children[0]
-        icon.addEventListener('click', () => {
-            if (input.getAttribute('type') === "password") {
-                input.setAttribute('type', 'text')
-                icon.nextElementSibling.style = "display: initial"
-                icon.style = "display: none"
-            } else {
-                input.setAttribute('type', 'password')
-                icon.style = "display: none";
-                input.nextElementSibling.style = "display: initial"
-            }
-        })
+  const passwIcons = document.querySelectorAll('[data-icon]')
+  passwIcons.forEach((icon) => {
+    const input = icon.parentElement.children[0]
+    icon.addEventListener('click', () => {
+      if (input.getAttribute('type') === "password") {
+        input.setAttribute('type', 'text')
+        icon.nextElementSibling.style = "display: initial"
+        icon.style = "display: none"
+      } else {
+          input.setAttribute('type', 'password')
+          icon.style = "display: none";
+          input.nextElementSibling.style = "display: initial"
+        }
     })
+  })
 } 
